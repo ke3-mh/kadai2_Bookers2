48c401b36fec9d52407ee8d72a917ed1811764d2
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-01-21 06:51:53 +0000

    [Update] 4-10_hukushuu

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  
+  get 'homes/about' => 'homes#about', as: "about"
+  resources :users, only: [:show, :edit]
+  resources :books, only: [:new, :index, :show, :edit]
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
