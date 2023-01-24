688a4631d4f3931d249d811d8edd79d3fdadcffa
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-01-24 02:06:08 +0000

    [Update] User一覧機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'homes/about' => 'homes#about', as: "about"
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:index, :show, :edit]
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
