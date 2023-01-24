48c26cfd2315963fde641ec5ccaf966a044e2c33
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-01-24 15:01:58 +0000

    [Update]Userコントローラ機能実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,32 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
     @book = @user.books
+    @newbook = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def book_params
+    params.require(:book).permit(:title ,:body)
+  end
+
 end
