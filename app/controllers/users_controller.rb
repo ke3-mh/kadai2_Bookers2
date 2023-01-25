ea94609b9ff0ed15e635caa495779e71157dd2d9
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-01-25 15:00:47 +0000

    [Update]機能実装(checkシート37項までの機能)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+
+    @user = current_user
+    @newbook = Book.new
   end
 
   def show
     @user = User.find(params[:id])
-    @book = @user.books
     @newbook = Book.new
+
+    @book = @user.books
   end
 
   def edit
