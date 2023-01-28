1c293f7b2c1bc5fd63aabf731b23107244e59775
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-01-28 09:01:55 +0000

    [Update]Validate実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,15 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    flash[:booknew] = "You have created book successfully."
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:booknew] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @books = Book.all
+      @newbook = Book.new
+      render :index
+    end
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :body, 
+  validates :body,
     presence: true,
     length: { maximum: 200 }
 end
