7792fd6ae791316c4bc3d8b5425aed3513b9e4a5
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-01-30 15:15:10 +0000

    [Fix]Aboutリンク修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  get 'homes/about' => 'homes#about', as: "about"
+  get 'home/about' => 'homes#about', as: "about"
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 
